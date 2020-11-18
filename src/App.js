77a4c9f7566f86b1a96da2090a3f5306abdae357
bohaess <bohaess1701@gmail.com>
2020-11-18 06:00:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import './style.css'
+//import imageInPublic from '../public/imageInPublic.jpg'
+import imageInSrc from './imageInSrc.jpg'
+//import myVideo from '/myVideo.mp4'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+        
+    <div className="cont">
+       <div style={{border:"1px solid black",maxWidth:"100vw"}} className="div">
+         <h1 className="title red"> Origami :</h1>
+         <h3> Phone Stand / Holder !!</h3>
+         <div className="image-container">
+         <img src={imageInSrc} alt="image in src" className="img"/>
+         <img src={process.env.PUBLIC_URL + "/imageInPublic.jpg"} alt="mypic" className="img"/>
+         </div>
+       </div>
+       <video style={{width:"400px",height:"420px"}} controls className="video" poster="./origami.jpg">
+         <source src={process.env.PUBLIC_URL + "/myVideo.mp4"} />
+       </video>
+   </div>
+    
+  )
 }
 
 export default App;
